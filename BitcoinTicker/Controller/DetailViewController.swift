466ead78388bd3490e42a1934fb7ab4acf0f1287
Harshavardhan K <harshavardhan816@gmail.com>
2018-04-07 20:58:22 +0530

    Trial Commit

--- BitcoinTicker/Controller/DetailViewController.swift
+++ BitcoinTicker/Controller/DetailViewController.swift
@@ -2,8 +2,8 @@
 //  DetailTableViewController.swift
 //  BitcoinTicker
 //
-//  Created by Harshavardhan K on 27/02/18.
-//  Copyright © 2018 London App Brewery. All rights reserved.
+//  Created by Harshavardhan K on 26/02/18.
+//  Copyright © 2018 Harshavardhan K. All rights reserved.
 //
 
 import UIKit
--- BitcoinTickerTests/BitcoinTickerTests.swift
+++ BitcoinTickerTests/BitcoinTickerTests.swift
@@ -3,7 +3,7 @@
 //  BitcoinTickerTests
 //
 //  Created by Harshavardhan K on 26/02/18.
-//  Copyright © 2018 London App Brewery. All rights reserved.
+//  Copyright © 2018 Harshavardhan K. All rights reserved.
 //
 
 import XCTest
